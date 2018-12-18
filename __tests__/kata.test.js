6a641b50e94996d379490f1ebd5a593ed16a9cf5
Lyla Fibert <lylafibert@MacBook-Pro-6.local>
2018-12-18 13:40:43 +0000

    more katas

--- __tests__/kata.test.js
+++ __tests__/kata.test.js
@@ -28,3 +28,41 @@ test("fib returns fibonacci sequence up to 4 million", () => {
 test("sumEvenFib returns the sum of the even fibonacci numbers below 4 million", () => {
     expect(kata.sumEvenFib()).toBe(4613732);
 });
+
+test("check isPalindrome correctly checks if a number is a palindrome or not", () => {
+    expect(kata.isPalindrome(35155153)).toBe(true);
+    expect(kata.isPalindrome(1598234)).toBe(false);
+    expect(kata.isPalindrome(3)).toBe(true);
+    expect(kata.isPalindrome(24)).toBe(false);
+    expect(kata.isPalindrome(906609)).toBe(true);
+});
+
+test("check largestPal returns the largest palindrome that is a product of two 3 digit numbers", () =>{
+    expect(kata.largestPal()).toBe(906609);
+});
+
+test("check 232792560 returns true in isDivis", () => {
+    expect(kata.isDivis(232792560)).toBe(true);
+    expect(kata.isDivis(4)).toBe(false);
+});
+
+test("check smallDivis returns 23279256", () =>{
+    expect(kata.smallDivis()).toBe(232792560);
+});
+
+test("check sumOfSquares returns right answer", () => {
+    expect(kata.sumOfSquares()).toBe(338350);
+});
+
+test("check squareOfSums returns right answer", () => {
+    expect(kata.squareOfSums()).toBe(25502500);
+});
+
+test("test if checkPrime works", () => {
+    expect(kata.checkPrime(3)).toBe(true);
+    expect(kata.checkPrime(9)).toBe(false);
+    expect(kata.checkPrime(2)).toBe(true);
+    expect(kata.checkPrime(0)).toBe(false);
+    expect(kata.checkPrime(1)).toBe(false);
+
+});
--- kata.js
+++ kata.js
@@ -1,87 +1,207 @@
-
 const listNum = () => {
-    let numbers = [];
-    for (let i = 1; i < 1000; i++) {
-        if (i % 3 === 0 || i % 5 === 0) {
-            numbers.push(i);
-        }
-    };
+  let numbers = [];
+  for (let i = 1; i < 1000; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      numbers.push(i);
+    }
+  }
 
-    let sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue);
+  let sum = numbers.reduce(
+    (accumulator, currentValue) => accumulator + currentValue
+  );
 
-
-    return sum;
+  return sum;
 };
 
 const fib = () => {
-    let fibArray = [];
-    fibArray[0] = 0;
-    fibArray[1] = 1;
-    for (i = 2; fibArray[i - 1] < 4000000; i++) {
-        fibArray[i] = fibArray[i - 2] + fibArray[i - 1];
-    };
-    return fibArray;
+  let fibArray = [];
+  fibArray[0] = 0;
+  fibArray[1] = 1;
+  for (i = 2; fibArray[i - 1] < 4000000; i++) {
+    fibArray[i] = fibArray[i - 2] + fibArray[i - 1];
+  }
+  return fibArray;
 };
 
 const evenFib = () => {
-    const even = fib().filter(number => number % 2 === 0);
-    return even;
+  const even = fib().filter(number => number % 2 === 0);
+  return even;
 };
 
 const sumEvenFib = () => {
-    const sum = evenFib().reduce((accumulator, currentValue) => accumulator + currentValue);
-    return sum;
+  const sum = evenFib().reduce(
+    (accumulator, currentValue) => accumulator + currentValue
+  );
+  return sum;
 };
 
-const checkPrime = (num) => {
-    for (let i = 2; i < num; i++) {
-        if (num % i === 0) {
-            return false
-        } else if (num === 0 || num === 1) {
-            return false
-        } else {
-            return true;
-        }
+const checkPrime = num => {
+  if (num === 0 || num === 1) {
+    return false;
+  }
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;
     }
+  }
+  return true;
 };
 
 const largeFactor = () => {
-    const bigNum = 600851475143;
-    
-    for (let i = bigNum - 1; i > 0; i--) {
+  const bigNum = 600851475143;
 
-        if (bigNum % i === 0) {
-            if(checkPrime(i)) {
-                return i;
-            }   
-        }
-    };
-    return 0;
-
+  for (let i = bigNum - 1; i > 0; i--) {
+    if (bigNum % i === 0) {
+      if (checkPrime(i)) {
+        return i;
+      }
+    }
+  }
+  return 0;
 };
 
-const highFactor = () =>{
-let originalTarget = 600851475143;
-let target = originalTarget;
-let i = 2;
-while (i < target) {
+const highFactor = () => {
+  let originalTarget = 600851475143;
+  let target = originalTarget;
+  let i = 2;
+  while (i < target) {
     while (target % i === 0) {
-        (function (newtarget) {
-            target = newtarget;
-        })(target / i)
+      (function(newtarget) {
+        target = newtarget;
+      })(target / i);
     }
     i++;
-}
-console.log(target);
-return target;
+  }
+  console.log(target);
+  return target;
 };
 
+//Problem 4
 
-highFactor();
+const isPalindrome = num => {
+  const number = num.toString();
+  if (
+    number ===
+    number
+      .split("")
+      .reverse()
+      .join("")
+  ) {
+    return true;
+  }
+  return false;
+};
 
+const largestPal = () => {
+  let max = 0;
+
+  for (let i = 999; i >= 100; i--) {
+    for (let j = i; j >= 100; j--) {
+      let product = i * j;
+
+      if (isPalindrome(product)) {
+        if (max < product) {
+          max = product;
+        }
+      }
+    }
+  }
+  console.log(max);
+  return max;
+};
+
+//problem 5
+
+const isDivis = num => {
+  let i = 1;
+  while (i < 21) {
+    if (num % i !== 0) {
+      return false;
+    }
+    i++;
+  }
+  return true;
+};
+
+const smallDivis = () => {
+  let i = 1;
+  while (!isDivis(i)) {
+    i++;
+  }
+  console.log(i);
+  return i;
+};
+
+//Problem 6
+
+const sumOfSquares = () => {
+  let sum = 0;
+  for (i = 1; i < 101; i++) {
+    sum = sum + i * i;
+  }
+  return sum;
+};
+
+const squareOfSums = () => {
+  let sum = 0;
+  let i = 1;
+  while (i <= 100) {
+    sum += i;
+    i++;
+  }
+  return sum * sum;
+};
+
+const diff = () => {
+  return squareOfSums() - sumOfSquares();
+};
+
+// By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+// What is the 10 001st prime number?
+
+const prime = () => {
+  const primes = [];
+  let i = 0;
+  while (primes.length < 10002) {
+    if (checkPrime(i)) {
+      primes.push(i);
+    }
+    i++;
+  }
+  return primes[10000];
+};
+
+//problem 8
+const thirteen = () => {
+  const number = '7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450';
+  let max = 0;
+  console.log(number)
+  let product = 0;
+
+  for (let i = 0; i < number.length - 13; i++) {
+    const smallArray = number.slice(i, i + 13);
+    product = smallArray[0];
+    for (let j = 1; j < smallArray.length; j++) {
+      product = product * smallArray[j];
+      if (max < product) {
+        max = product;
+      }
+    }
+  }
+  console.log(max);
+};
+
+thirteen();
 
 module.exports = {
-    listNum: listNum,
-    fib: fib,
-    sumEvenFib: sumEvenFib
+  listNum: listNum,
+  fib: fib,
+  sumEvenFib: sumEvenFib,
+  isPalindrome: isPalindrome,
+  largestPal: largestPal,
+  isDivis: isDivis,
+  smallDivis: smallDivis,
+  sumOfSquares: sumOfSquares,
+  squareOfSums: squareOfSums,
+  checkPrime: checkPrime
 };
